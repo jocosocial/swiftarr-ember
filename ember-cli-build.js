e9b08425bf904cddfd431eac02eb7b77c2884e05
hendusoone <hendricksond@gmail.com>
2021-12-10 15:42:03 -0500

    Add boostrap, clean app template

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    sassOptions: {
+      includePaths: ['node_modules/bootstrap/scss'],
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -20,5 +23,7 @@ module.exports = function (defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js');
+  
   return app.toTree();
 };
